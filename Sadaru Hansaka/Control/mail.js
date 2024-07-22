5821ef6fe3a2ca1b026c52cb3db31c0b7403f9a2
Sadaru-Hansaka <sandaruhansaka0828@gmail.com>
2024-07-22 23:31:12 +0530

    modified contact page and set up to get emails from front end to mail

---
+++ Sadaru Hansaka/Control/mail.js
@@ -0,0 +1,31 @@
+import { getPW } from "./keys";
+
+const form = document.querySelector('form');
+const name = document.getElementById('name');
+const mail = document.getElementById('email');
+const messege = document.getElementById('message');
+const subject = document.getElementById('subject');
+
+
+
+function sendEmail() {
+    const msg = `Name : ${name.value}<br> Email : ${mail.value}<br><br> ${messege.value}`
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "hansakas305@gmail.com",
+        Password : getPW(),
+        To : 'hansakas305@gmail.com',
+        From : "hansakas305@gmail.com",
+        Subject : subject.value,
+        Body : msg,
+    }).then(
+      message => alert(message)
+    );
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    sendEmail();
+    this.reset();
+})
